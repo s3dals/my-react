d216cc2f9f47d798049051ea16e320f1b8798f11
S3d-ALS <98984673+s3dals@users.noreply.github.com>
2023-03-06 20:12:10 +0100

    Update MainHeader.jsx

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -10,7 +10,6 @@ function MainHeader({ onCreatePost }) {
         React Poster
       </h1>
       <p>
-      
         <button className={classes.button} onClick={onCreatePost}>
           <MdPostAdd size={18} />
           New Post
